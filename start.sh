bce90331f0f255d9ca824866be17871380756764
John Castberg <jpcastberg@gmail.com>
2025-04-06 11:26:52 -0700

    disable telemetry, update next, fix cron pid permissions

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ log_file="/var/log/bayareashows.log"
     echo "Starting bayareashows.com..."
     cd /app
     npm install
+    npx next telemetry disable
     npm run build
     npm run start
 } 2>&1 | tee -a $log_file
